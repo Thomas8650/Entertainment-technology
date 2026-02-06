735417973815c7674cf5b7b09c7ca7b3d4c772a8
Thomas Demeulenaere <demeulenaerethomas21@gmail.com>
2026-02-06 14:09:39 +0100

    update

---
+++ opdracht 1/opdracht1/MDK-ARM/opdracht1/midi_device.d
@@ -0,0 +1,19 @@
+opdracht1/midi_device.o: \
+  ..\MiddlewaresThird_PartyTinyUSB\src\class\midi\midi_device.c \
+  ..\MiddlewaresThird_PartyTinyUSB\src\tusb_option.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_compiler.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\tusb_config.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_mcu.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\device\usbd.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_common.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_verify.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_types.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_debug.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\device\usbd_pvt.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\osal\osal.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\osal\osal_none.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_fifo.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_private.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\class\midi\midi_device.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\class\audio\audio.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\class\midi\midi.h
---
+++ opdracht 1/opdracht1/MDK-ARM/opdracht1/usbd.d
@@ -0,0 +1,20 @@
+opdracht1/usbd.o: ..\MiddlewaresThird_PartyTinyUSB\src\device\usbd.c \
+  ..\MiddlewaresThird_PartyTinyUSB\src\tusb_option.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_compiler.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\tusb_config.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_mcu.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\device\dcd.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_common.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_verify.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_types.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_debug.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\osal\osal.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\osal\osal_none.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_fifo.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\tusb.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\device\usbd.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\class\midi\midi_device.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\class\audio\audio.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\class\midi\midi.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\common\tusb_private.h \
+  ..\MiddlewaresThird_PartyTinyUSB\src\device\usbd_pvt.h
